009cf529bc3d905cbe15799d315398dc8b55f9b0
MohammadYasir <m.yasir0001@gmail.com>
2019-03-24 10:25:43 +0530

    Hibernate Inheritance Mapping Single Table

---
+++ inheritance-mapping/single-table/src/main/java/com/example/singletable/App.java
@@ -0,0 +1,87 @@
+package com.example.singletable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import com.example.singletable.tables.Car;
+import com.example.singletable.tables.Truck;
+import com.example.singletable.tables.Vehicle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static SessionFactory factory;
+	
+	/*
+	 * Initializing the Hibernate static factory instance.
+	 */
+	static {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(Vehicle.class);
+		cfg.addAnnotatedClass(Car.class);
+		cfg.addAnnotatedClass(Truck.class);
+		ServiceRegistry srv = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
+		factory = cfg.buildSessionFactory(srv);
+	}
+	
+    public static void main( String[] args )
+    {
+    	Session session = null;
+        try {
+			session = factory.openSession();
+			Transaction tx = session.beginTransaction();
+			
+			Vehicle vehicle1 = new Vehicle();
+			vehicle1.setManufactureYear(2012);
+			vehicle1.setNoOfWheels(3);
+			
+			Vehicle vehicle2 = new Vehicle();
+			vehicle2.setManufactureYear(2013);
+			vehicle2.setNoOfWheels(3);
+			
+			Car car1 = new Car();
+			car1.setManufactureYear(2014);
+			car1.setNoOfWheels(4);
+			car1.setModel("Benz");
+			car1.setSeatingCapacity(5);
+			
+			Car car2 = new Car();
+			car2.setManufactureYear(2015);
+			car2.setNoOfWheels(4);
+			car2.setModel("Ford Aspire");
+			car2.setSeatingCapacity(5);
+			
+			Truck truck1 = new Truck();
+			truck1.setManufactureYear(2016);
+			truck1.setNoOfWheels(4);
+			truck1.setMaxPayload(1500);
+			truck1.setContainerized(false);
+			
+			Truck truck2 = new Truck();
+			truck2.setManufactureYear(2017);
+			truck2.setNoOfWheels(4);
+			truck2.setMaxPayload(1800);
+			truck2.setContainerized(true);
+			
+			session.save(vehicle1);
+			session.save(vehicle2);
+			session.save(car1);
+			session.save(car2);
+			session.save(truck1);
+			session.save(truck2);
+			
+			tx.commit();
+			
+		} finally {
+			session.close();
+		}
+    }
+}
---
+++ inheritance-mapping/single-table/src/main/java/com/example/singletable/tables/Truck.java
@@ -0,0 +1,25 @@
+package com.example.singletable.tables;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("truck")
+public class Truck extends Vehicle {
+
+	private int maxPayload;
+	private boolean isContainerized;
+	
+	public int getMaxPayload() {
+		return maxPayload;
+	}
+	public void setMaxPayload(int maxPayload) {
+		this.maxPayload = maxPayload;
+	}
+	public boolean isContainerized() {
+		return isContainerized;
+	}
+	public void setContainerized(boolean isContainerized) {
+		this.isContainerized = isContainerized;
+	}
+}
